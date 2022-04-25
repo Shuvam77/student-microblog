df79c155569f15e2fa0f5370a80e242f46d80a33
shuvam77 <shuvam77@gmail.com>
2022-04-25 11:12:01 +0200

    some custom permission

--- base/admin.py
+++ base/admin.py
@@ -4,10 +4,25 @@ from base.models import Room, Topic, Message
 
 # Register your models here.
 
+class CustomPermissionMixin:
+    def get_form(self, request, obj=None, **kwargs):
+        form = super().get_form(request, obj, **kwargs)
+        is_superuser = request.user.is_superuser
 
-class RoomAdmin(admin.ModelAdmin):
-    list_disply = ('name', 'description', 'created', 'updated')
+        if not is_superuser:
+            form.base_fields['name'].disabled = True
+        return form
+
+    def has_delete_permission(self, request, obj=None):
+        return False
+    def has_add_permission(self, request):
+        return False
+
+
+class RoomAdmin(CustomPermissionMixin, admin.ModelAdmin):
+    list_disply = ('name', 'description')
     readonly_field = ('created', 'updated')
+
 admin.site.register(Room, RoomAdmin)
 
 admin.site.register(Topic)
