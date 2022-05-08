e6e7869784ae858e23b27ab8cb4c8ce7b141e5d5
shuvam77 <shuvam77@gmail.com>
2022-05-08 17:01:10 +0200

    api for room added

--- api/urls.py
+++ api/urls.py
@@ -1,8 +1,11 @@
 from django.urls import path
 
-from api.views import RoomAPIView
+from api.views import RoomAPIView, RoomDetailAPIView, RoomDeleteAPIView, RoomCreateAPIView
 
 
 urlpatterns = [
-    path('', RoomAPIView.as_view(), name="room:apiroom_list")
+    path('', RoomAPIView.as_view(), name="room:apiroom_list"),
+    path('<int:pk>', RoomDetailAPIView.as_view(), name="room:apiroom_detail"),
+    path('del/<int:pk>', RoomDeleteAPIView.as_view(), name="room:apiroom_delete"),
+    path('create', RoomCreateAPIView.as_view(), name="room:apiroom_create" )
 ]
--- api/views.py
+++ api/views.py
@@ -1,13 +1,41 @@
-from django.shortcuts import render
-
+from django.shortcuts import get_object_or_404, render
 from rest_framework import generics
 from base.models import User, Topic, Room, Message
 from .serializers import RoomSerializer
+from rest_framework.permissions import IsAuthenticated
 
 # Create your views here.
 
 class RoomAPIView(generics.ListAPIView):
-    queryset = Room.objects.all()
+    queryset = Room.objects.all().order_by('-created')
     serializer_class = RoomSerializer
+
+
+class RoomDetailAPIView(generics.RetrieveAPIView):
+    serializer_class = RoomSerializer
+
+    def get_queryset(self):
+        return Room.objects.all()
+
+
+class RoomDeleteAPIView(generics.DestroyAPIView):
+    serializer_class = RoomSerializer
+    permission_classes = [IsAuthenticated]
+
+    def get_queryset(self):
+        queryset = Room.objects.filter(host = self.request.user.id)
+        return queryset
+
+
+class RoomCreateAPIView(generics.CreateAPIView):
+    serializer_class = RoomSerializer
+    permission_classes = [IsAuthenticated]
+    # queryset = Room.objects.all()
+
+    def perform_create(self, serializer):
+        host = get_object_or_404(User, id=self.request.user.id)
+        return serializer.save(host = host)
+
+
 
 
